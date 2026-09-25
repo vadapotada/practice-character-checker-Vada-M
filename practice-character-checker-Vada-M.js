11e09f45befcc0043ded7ae12f34734cc22aae9a
vadapotada <vadameadows@gmail.com>
2026-09-24 21:05:48 -0500

    Added code to ask the user for a word, and to then index/count that word's characters. Terminal returns correct character index with bracket notation.

--- practice-character-checker-Vada-M.js
+++ practice-character-checker-Vada-M.js
@@ -1,1 +1,6 @@
+const readlineSync = require("readline-sync");
 
+let interpretedWord = readlineSync.question("Enter any word you want, please: ");
+let indexCount = readlineSync.question("Enter a number to determine the index. ");
+
+console.log(interpretedWord[indexCount]);
